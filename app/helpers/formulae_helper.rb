d74d9cc3309fba0546210dacc8c5611b1cdf3f0a
Sebastian Staudt <koraktor@gmail.com>
2018-04-26 23:41:34 +0200

    Minimize queries in formula helpers

--- app/helpers/formulae_helper.rb
+++ app/helpers/formulae_helper.rb
@@ -1,19 +1,19 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2017, Sebastian Staudt
+# Copyright (c) 201-2018, Sebastian Staudt
 
 require 'digest'
 
 module FormulaeHelper
 
   def code_link(formula, revision)
-    link_to 'Formula code', "https://github.com/#{formula.repository.name}/blob/#{revision.sha}/#{formula.path}", target: :_blank
+    link_to 'Formula code', "https://github.com/#{formula.repository_id}/blob/#{revision.sha}/#{formula.path}", target: :_blank
   end
 
   def formula_diff_link(formula, rev)
     diff_md5 = Digest::MD5.hexdigest formula.path
-    link_to '', "https://github.com/#{formula.repository.name}/commit/#{rev.sha}#diff-#{diff_md5}", target: :_blank
+    link_to '', "https://github.com/#{formula.repository_id}/commit/#{rev.sha}#diff-#{diff_md5}", target: :_blank
   end
 
   def feed_link
@@ -23,7 +23,7 @@ module FormulaeHelper
   end
 
   def history_link(formula)
-    link_to 'Complete formula history at GitHub', "https://github.com/#{formula.repository.name}/commits/HEAD/#{formula.path}", target: :_blank
+    link_to 'Complete formula history at GitHub', "https://github.com/#{formula.repository_id}/commits/HEAD/#{formula.path}", target: :_blank
   end
 
   def letters
