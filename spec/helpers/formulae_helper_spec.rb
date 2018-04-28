2c742f8832989ffa1fe53db7b9a416031bbf1dd8
Sebastian Staudt <koraktor@gmail.com>
2018-04-28 14:31:44 +0200

    Fix formulae helper specs

--- spec/helpers/formulae_helper_spec.rb
+++ spec/helpers/formulae_helper_spec.rb
@@ -1,7 +1,7 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2017, Sebastian Staudt
+# Copyright (c) 2017-2018, Sebastian Staudt
 
 require 'rails_helper'
 
@@ -10,7 +10,7 @@ describe FormulaeHelper do
   describe '#code_link' do
     it 'returns a correct link to commit’s formula code' do
       formula = mock path: 'Formula/git.rb',
-                     repository: mock(name: 'Homebrew/homebrew-core')
+                     repository_id: 'Homebrew/homebrew-core'
       revision = mock sha: 'deadbeef'
 
       expect(helper.code_link(formula, revision)).to eq('<a target="_blank" href="https://github.com/Homebrew/homebrew-core/blob/deadbeef/Formula/git.rb">Formula code</a>')
@@ -35,7 +35,7 @@ describe FormulaeHelper do
   describe '#formula_diff_link' do
     it 'returns a correct link to the commit’s diff of the formula' do
       formula = mock path: 'Formula/git.rb',
-                     repository: mock(name: 'Homebrew/homebrew-core')
+                     repository_id: 'Homebrew/homebrew-core'
       revision = mock sha: 'deadbeef'
 
       expect(helper.formula_diff_link(formula, revision)).to eq('<a target="_blank" href="https://github.com/Homebrew/homebrew-core/commit/deadbeef#diff-3e84bae646d908b93e043833873d316d"></a>')
@@ -45,7 +45,7 @@ describe FormulaeHelper do
   describe '#history_link' do
     it 'returns a correct link to the formula’s history on GitHub' do
       formula = mock path: 'Formula/git.rb',
-                     repository: mock(name: 'Homebrew/homebrew-core')
+                     repository_id: 'Homebrew/homebrew-core'
 
       expect(helper.history_link(formula)).to eq('<a target="_blank" href="https://github.com/Homebrew/homebrew-core/commits/HEAD/Formula/git.rb">Complete formula history at GitHub</a>')
     end
